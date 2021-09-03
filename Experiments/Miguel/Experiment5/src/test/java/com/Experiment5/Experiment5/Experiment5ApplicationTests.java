7dc34d0af5e341892709c7965943be2885f2b25a
miguelmartinn9 <nerviosus@protonmail.com>
2021-09-03 10:34:33 -0500

    Pass data from one activity to another

---
+++ Experiments/Miguel/Experiment5/src/test/java/com/Experiment5/Experiment5/Experiment5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Experiment5.Experiment5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Experiment5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
