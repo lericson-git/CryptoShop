0ebddb4f0c25f1fb8d3ef10cbe9d697b6d6088bf
mjpatel <mjpatel@iastate.edu>
2021-09-07 11:26:32 -0500

    Spring Boot Experiment

---
+++ Experiments/Meet/springboot-helloworld1/springboot-helloworld1/src/test/java/com/javainuse/springboothelloworld1/SpringbootHelloworld1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainuse.springboothelloworld1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHelloworld1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
