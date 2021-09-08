1a7d2e0a8c3d4b468f427e4545a65b59afc1c29d
mjpatel <mjpatel@iastate.edu>
2021-09-08 15:34:05 -0500

    Set up the hello world experiment in android studio

---
+++ Experiments/Meet/spring-boot/spring-boot/src/main/java/com/example/springboot/Experiments23Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Experiments23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Experiments23Application.class, args);
+	}
+
+}
---
+++ Experiments/Meet/spring-boot/spring-boot/src/test/java/com/example/springboot/Experiments23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Experiments23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
