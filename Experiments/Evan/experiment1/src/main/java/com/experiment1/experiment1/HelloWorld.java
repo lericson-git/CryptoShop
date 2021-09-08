69ebcd41837d1777754994a2a867a8f8a561d6d7
mjpatel <mjpatel@iastate.edu>
2021-09-08 15:37:48 -0500

    added post method

--- Experiments/Evan/experiment1/src/main/java/com/experiment1/experiment1/HelloWorld.java
+++ Experiments/Evan/experiment1/src/main/java/com/experiment1/experiment1/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.experiment1.experiment1;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,15 @@ public class HelloWorld {
     @RequestMapping("/")
     public String getHelloWorld() {
         return "HELLO WORLD";
+    }
+
+    @RequestMapping(value = "/start", method = RequestMethod.GET)
+    public String read() {
+        return "start";
+    }
+
+    @PostMapping("/start")
+    public String create(){
+        return "start post";
     }
 }
