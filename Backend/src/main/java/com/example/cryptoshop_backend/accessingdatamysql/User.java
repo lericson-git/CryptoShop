02b11b6a3eced7822403441aa207dfc6794d7f4f
miguelmartinn9 <nerviosus@protonmail.com>
2021-09-24 09:47:20 -0500

    Modified User Entity to match with table fields

--- Backend/src/main/java/com/example/cryptoshop_backend/accessingdatamysql/User.java
+++ Backend/src/main/java/com/example/cryptoshop_backend/accessingdatamysql/User.java
@@ -10,10 +10,53 @@ public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
-
     private String name;
-
     private String email;
+    private String username;
+    private String hashed_pass;
+    private int bought_p;
+    private int sold_p;
+    private double btc_balance;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHashed_pass() {
+        return hashed_pass;
+    }
+
+    public void setHashed_pass(String hashed_pass) {
+        this.hashed_pass = hashed_pass;
+    }
+
+    public int getBought_p() {
+        return bought_p;
+    }
+
+    public void setBought_p(int bought_p) {
+        this.bought_p = bought_p;
+    }
+
+    public int getSold_p() {
+        return sold_p;
+    }
+
+    public void setSold_p(int sold_p) {
+        this.sold_p = sold_p;
+    }
+
+    public double getBtc_balance() {
+        return btc_balance;
+    }
+
+    public void setBtc_balance(double btc_balance) {
+        this.btc_balance = btc_balance;
+    }
 
     public Integer getId() {
         return id;
