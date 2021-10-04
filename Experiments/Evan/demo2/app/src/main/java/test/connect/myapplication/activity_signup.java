d3493702fec03034b359597447c253bd1a5d4422
lericson <lericson@iastate.edu>
2021-10-04 13:25:49 -0500

    Added SHA265 hashing for user pass inside of signup activity.

--- Experiments/Evan/demo2/app/src/main/java/test/connect/myapplication/activity_signup.java
+++ Experiments/Evan/demo2/app/src/main/java/test/connect/myapplication/activity_signup.java
@@ -6,14 +6,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import test.connect.myapplication.api.SlimCallback;
 import test.connect.myapplication.model.User;
 
 public class activity_signup extends AppCompatActivity {
+
+    //Encrypts string base into sha256 format
+    public String sha256(String base) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            StringBuilder hexString = new StringBuilder();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +59,7 @@ public class activity_signup extends AppCompatActivity {
                 newUser.setName(nameIn.getText().toString());
                 newUser.setEmail(emailIn.getText().toString());
                 newUser.setUsername(usernameIn.getText().toString());
-                newUser.setHashed_pass(passwordIn.getText().toString());
+                newUser.setHashed_pass(sha256(passwordIn.getText().toString()));
                 newUser.setBought_p(0);
                 newUser.setSold_p(0);
                 newUser.setBtc_balance(0);
