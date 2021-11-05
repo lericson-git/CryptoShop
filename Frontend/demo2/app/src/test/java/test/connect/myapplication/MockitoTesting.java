d22659ebd88b9f975d5d0fab382ee16d4c9e6728
mjpatel <mjpatel@iastate.edu>
2021-11-05 14:51:31 -0500

    Additional Testing

---
+++ Frontend/demo2/app/src/test/java/test/connect/myapplication/MockitoTesting.java
@@ -0,0 +1,63 @@
+package test.connect.myapplication;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+import test.connect.myapplication.model.Post;
+
+
+public class MockitoTesting {
+    @Test
+    // Shouldn't be listed before post is created
+    public void test_bothListedBefore() {
+        activity_sell spySell = spy(activity_sell.class);
+        assertEquals(spySell.getListed(), Boolean.FALSE);
+    }
+    @Test
+    // Should be listed after post is created
+    public void test_bothListedAfter() {
+        activity_sell spySell = spy(activity_sell.class);
+        Post ex = new Post("Novel", 20, "Shakespear Drama");
+
+        spySell.setPost(ex);
+        verify(spySell).setPost(ex);
+        assertEquals(spySell.getListed(), Boolean.TRUE);
+    }
+    @Test
+
+    public void test_samePost() {
+        activity_sell spySell = spy(activity_sell.class);
+        Post ex = new Post("Drama", 20, "Wizard and Oz");
+
+        spySell.setPost(ex);
+        verify(spySell).setPost(ex);
+        assertEquals(spySell.getPost(), ex);
+    }
+    @Test
+
+    public void test_notsamePost() {
+        activity_sell spySell = spy(activity_sell.class);
+        Post ex = new Post("Book", 50, "Huck Finn: Adventure");
+
+        spySell.setPost(ex);
+        verify(spySell).setPost(ex);
+        assertEquals(spySell.getPost(), ex);
+    }
+
+
+
+
+}
+
+
+
